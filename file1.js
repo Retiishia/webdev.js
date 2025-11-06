78f3a0f93ce265bfd43b4606a39db45b1b6cbae5
Retiishia <azizfarhan72@gmail.com>
2025-11-07 01:58:42 +0700

    initial commit

---
+++ file1.js
@@ -0,0 +1,10 @@
+// const myName = "Farhan Aziz";
+// console.log(myName);
+
+const firstName = "Farhan";
+const lastName = "Aziz";
+const fullName = "Farhan Aziz";
+
+console.log(firstName);
+console.log(lastName);
+console.log(fullName);
