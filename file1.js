2772ebfc2a17f052812df5d84f7cb7bf89f1a54f
Retiishia <azizfarhan72@gmail.com>
2025-11-07 02:15:15 +0700

    Learn String, Next Number

--- file1.js
+++ file1.js
@@ -1,10 +1,10 @@
-// const myName = "Farhan Aziz";
-// console.log(myName);
-
 const firstName = "Farhan";
 const lastName = "Aziz";
-const fullName = "Farhan Aziz";
+const fullName = `${firstName} ${lastName}`;
+const food = "Burger"
 
+
+console.log("Halo Namaku " + fullName + " dan makanan favoriteku adalah " + food);
 console.log(firstName);
 console.log(lastName);
 console.log(fullName);
