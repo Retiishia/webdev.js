dc6ed5582084103d60c5e1595cc39d6787b738bc
Retiishia <azizfarhan72@gmail.com>
2025-11-11 22:40:03 +0700

    feat: Learning Conditional IF/Else

---
+++ conditional.js
@@ -0,0 +1,15 @@
+// const hasPassport = false;
+
+// if (hasPassport) {
+//     console.log("You can travel abroad to any country.");
+// } else {
+//     console.log("You need a passport to travel abroad, so make sure to apply for one.");
+// }
+
+const Products = false;
+
+if (Products) {
+    console.log("You have items in your cart.");
+}  else {
+    console.log("Your cart is empty, please add some products.");
+}
