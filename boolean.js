ab123247b0e6df61000ee30fe54003c841d2bc48
Retiishia <azizfarhan72@gmail.com>
2025-11-11 22:32:57 +0700

    feat: adding learn boolean

---
+++ boolean.js
@@ -0,0 +1,9 @@
+const hasChildren = false;
+const isStudent = false;
+const isEmployed = true;
+const canBicycle = true;
+const hasPets = false;
+
+const isTall = true;
+const canCook = true;
+const hasSiblings = true;
