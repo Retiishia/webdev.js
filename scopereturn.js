f60a19d6528e33acce049e8b2071786f2934cc57
Retiishia <azizfarhan72@gmail.com>
2025-11-18 01:38:02 +0700

    feat: Adding learning Scope & Return

---
+++ scopereturn.js
@@ -0,0 +1,22 @@
+const firstName = "Farhan";
+const lastName = "Aziz";
+
+function getFullName(firstName, lastName) {
+    const fullName = firstName + " " + lastName;
+
+    if(1 === 1) {
+        console.log(fullName);
+    }
+    return fullName;
+};
+getFullName(firstName, lastName);
+
+
+
+// CHALENNGE
+function getNumber (x, y) {
+  return x * y;
+}
+
+const result = getNumber(5, 10);
+console.log(result);
